fd6dd5ddcc6eed7640ec26d99788aebd8dcbfffa
jonkracht <jonkracht@gmail.com>
2022-02-13 12:10:46 -0500

    Feb2022  playlist push

--- backup-rb-playlists.sh
+++ backup-rb-playlists.sh
@@ -6,4 +6,4 @@ playlists="/home/jon/.local/share/rhythmbox/playlists.xml"
 saveDirectory="/home/jon/jk-playlists/"
 backupScript="/home/jon/jk-playlists/rhythmbox_playlist_to_m3u.py"
 
-exec python3 "$backupScript" -i "$playlists" -o "$save_dir"
+exec python3 "$backupScript" -i "$playlists" -o "$saveDirectory"
