357a1148f342ea519a23447ac1c6056a69e4878f
Jon Kracht <jonkracht@gmail.com>
2022-07-21 16:33:41 -0400

    Setting up on Pop

--- backup-rb-playlists.sh
+++ backup-rb-playlists.sh
@@ -2,8 +2,8 @@
 
 # Script to create backups of playlists created in Rhythmbox
 
-playlists="/home/jon/.local/share/rhythmbox/playlists.xml"
-saveDirectory="/home/jon/jk-playlists/"
-backupScript="/home/jon/jk-playlists/rhythmbox_playlist_to_m3u.py"
+playlists="$HOME/.local/share/rhythmbox/playlists.xml"
+saveDirectory="$HOME/repos/rhythmbox-playlist-backup/playlists"
+backupScript="$HOME/repos/rhythmbox-playlist-backup/rhythmbox_playlist_to_m3u.py"
 
 exec python3 "$backupScript" -i "$playlists" -o "$saveDirectory"
